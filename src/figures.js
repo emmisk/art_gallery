aa7e256b4b5ed3693f9e77b99fecea9c09d69d5d
emmisk <koistinenemmisofia@gmail.com>
2020-08-24 20:51:08 +0300

    edit images names and change title

--- src/figures.js
+++ src/figures.js
@@ -1,7 +1,7 @@
 import Yellow_child from "./images/yellow_child.jpg"
 import Purple_child from "./images/purple_child.jpg"
 import Disco from "./images/disco.jpg"
-import Matthew from "./images/matthew_calane.jpg"
+import Matthew from "./images/matthew_clavane.jpg"
 import Cyclist from "./images/cyclist.jpg"
 import Twins_yellow from "./images/twins_yellow.jpg"
 import Twins_blue from "./images/twins_blue.jpg"
@@ -11,42 +11,42 @@ export const figures = [
     id: 0,
     src: Yellow_child,
     alt: "Yellow and child",
-    caption: "Acrylic painting",
+    caption: "Fear 2020",
   },
   {
     id: 1,
     src: Purple_child,
     alt: "Purple and child",
-    caption: "Acrylic painting",
+    caption: "Fear 2020",
   },
   {
     id: 2,
     src: Cyclist,
     alt: "Cyclist",
-    caption: "Acrylic painting",
+    caption: "Cyclist and cat 2019",
   },
   {
     id: 3,
     src: Disco,
     alt: "Disco",
-    caption: "Acrylic painting",
+    caption: "дискотека 2020",
   },
   {
     id: 4,
     src: Matthew,
-    alt: "Matthew Calane",
-    caption: "Digital draw",
+    alt: "Matthew Clavane",
+    caption: "Matthew Clavane 2020",
   },
   {
     id: 5,
     src: Twins_yellow,
     alt: "Yellow and twins",
-    caption: "Acrylic painting",
+    caption: "Sinä ja minä, kaksin 2020",
   },
   {
     id: 6,
     src: Twins_blue,
     alt: "Blue and twins",
-    caption: "Acrylic painting",
+    caption: "Sinä ja minä, kaksin 2020",
   },
 ]
