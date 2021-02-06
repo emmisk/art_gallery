04b1c217943463a85fa4dfef970b48e3f022c7ec
emmisk <koistinenemmisofia@gmail.com>
2021-02-06 15:40:09 +0200

    remove space

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Figure = ({ figure }) => (
   <figure>
-    <img src={figure.src} alt={figure.alt} />
+    <img src={figure.src} alt={figure.alt}/>
     <figcaption>{figure.caption}</figcaption>
   </figure>
 )
