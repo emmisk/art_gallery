7d32fa7bfa31884221d599913019fac6e02d1fe9
emmisk <koistinenemmisofia@gmail.com>
2021-02-06 15:36:47 +0200

    add toggleGallery, button and a pic of me

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Gallery from "./components/Gallery"
 import Button from "./components/Button"
 import Info from "./components/Info"
 import Contact from "./components/Contact"
+import MyImg from "./images/myImg.jpg"
 
 function App() {
   const [show, setShow] = useState(null)
-
+  
   const toggleInfo = () => (show === "info" ? setShow(null) : setShow("info"))
   const toggleContact = () =>
     show === "contact" ? setShow(null) : setShow("contact")
-
-  const scrollTop = () => window.scrollTo({ top: 0, behavior: "smooth" })
+    const toggleGallery = () =>
+    show === "gallery" ? setShow(null) : setShow("gallery")
 
   return (
     <div className="app">
@@ -20,11 +21,12 @@ function App() {
       <div className="buttons">
         <Button handleClick={toggleInfo} text="About me" />
         <Button handleClick={toggleContact} text="Contact me" />
+        <Button handleClick={toggleGallery} text="My works" />
       </div>
       {show === "info" && <Info />}
       {show === "contact" && <Contact />}
-      <Gallery />
-      <Button handleClick={scrollTop} text="Scroll top" />
+      {show === "gallery" && <Gallery />}
+      <img src={MyImg} alt="me"/>
     </div>
   )
 }
