2734a1e3f5416c2df2d19b07dfafad7014744246
emmisk <koistinenemmisofia@gmail.com>
2021-02-06 15:38:33 +0200

    edit header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 const Header = () => {
   return (
     <header>
-      <h1>Emmi's Art Gallery</h1>
+      <h1>Em<span className="headerSecondColor">m</span>i's Art Gal<span className="headerSecondColor">l</span>ery</h1>
     </header>
   )
 }
