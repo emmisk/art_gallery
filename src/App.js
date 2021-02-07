ba7d748677d40f01e4015d513c16db744e41277c
emmisk <koistinenemmisofia@gmail.com>
2021-02-07 18:18:00 +0200

    edit buttons order

--- src/App.js
+++ src/App.js
@@ -4,28 +4,28 @@ import Gallery from "./components/Gallery"
 import Button from "./components/Button"
 import Info from "./components/Info"
 import Contact from "./components/Contact"
-import MyImg from "./images/myImg.jpg"
+import MyImg from "./images/myPic.jpg"
 
 function App() {
   const [show, setShow] = useState(null)
   
   const toggleInfo = () => (show === "info" ? setShow(null) : setShow("info"))
+  const toggleGallery = () =>
+  show === "gallery" ? setShow(null) : setShow("gallery")
   const toggleContact = () =>
-    show === "contact" ? setShow(null) : setShow("contact")
-    const toggleGallery = () =>
-    show === "gallery" ? setShow(null) : setShow("gallery")
+  show === "contact" ? setShow(null) : setShow("contact")
 
   return (
     <div className="app">
       <Header />
       <div className="buttons">
         <Button handleClick={toggleInfo} text="About me" />
-        <Button handleClick={toggleContact} text="Contact me" />
         <Button handleClick={toggleGallery} text="My works" />
+        <Button handleClick={toggleContact} text="Contact me" />
       </div>
       {show === "info" && <Info />}
-      {show === "contact" && <Contact />}
       {show === "gallery" && <Gallery />}
+      {show === "contact" && <Contact />}
       <img src={MyImg} alt="me"/>
     </div>
   )
