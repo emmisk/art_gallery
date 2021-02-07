bf0681f2042afc8500982fdf3339dcf42817de04
emmisk <koistinenemmisofia@gmail.com>
2021-02-07 18:18:52 +0200

    edit info text

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,14 +2,15 @@ import React from "react"
 
 const Info = () => (
   <p>
-    <span className="field">Name:</span> Emmi.
+    <span className="field">Name:</span> Emmi Koistinen.
     <br />
     <span className="field">From:</span> Helsinki, Finland.
     <br />
     <span className="field">Who:</span> Amateur freetime artist.
     <br />
-    <span className="field">What:</span> I do acrylic and watercolor paintings
-    and some digital art with drawing board.
+    <span className="field">What:</span> I do acrylic and watercolor paintings 
+    and some digital art with drawing board. I also do logo clothes with 
+    fabric paints.
   </p>
 )
 
